a56555a743b0c9905585c513cfc83f936a6050c7
Batissta <francinaldofilho05@gmail.com>
2025-04-04 10:54:31 -0300

    refactor: UI finished

--- src/components/form/SignUpForm.tsx
+++ src/components/form/SignUpForm.tsx
@@ -1,15 +1,15 @@
 "use client";
-import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group";
-import React from "react";
+import { useState } from "react";
 import { Button } from "../ui/button";
 import LabelInput from "./LabelInput";
+import RadioType from "./RadioType";
 
 const SignUpForm = () => {
   // const [name, setName] = React.useState<string>("");
   // const [email, setEmail] = React.useState<string>("");
   // const [password, setPassword] = React.useState<string>("");
   // const [confirm, setConfirm] = React.useState<string>("");
-  const [type, setType] = React.useState<string>("passageiro");
+  const [type, setType] = useState<string>("passageiro");
 
   return (
     <form
@@ -45,74 +45,11 @@ const SignUpForm = () => {
         icon={"password"}
         placeholder="******"
       />
-      <RadioGroup className="grid grid-cols-2 gap-4">
-        <label
-          htmlFor="passageiro"
-          onClick={() => {
-            setType("passageiro");
-          }}
-          className={`rounded-2xl py-4 px-6 gap-2 text-zinc-50 checked:bg-zinc-900 checked:text-zinc-900 flex flex-col justify-center items-center ${
-            type !== "passageiro" ? "bg-zinc-200" : "bg-zinc-950"
-          }`}
-        >
-          <RadioGroupItem
-            value="passageiro"
-            id="passageiro"
-            checked={type === "passageiro"}
-            className="hidden"
-          />
-          <div className="flex flex-col">
-            <h1
-              className={`text-zinc-50 text-center ${
-                type !== "passageiro" ? "text-zinc-950" : "text-zinc-200"
-              }`}
-            >
-              Passageiro
-            </h1>
-            <span
-              className={`text-zinc-400 text-xs text-center ${
-                type !== "passageiro" ? "text-zinc-600" : "text-zinc-400"
-              }`}
-            >
-              Você veio buscar caronas
-            </span>
-          </div>
-        </label>
-
-        <label
-          htmlFor="motorista"
-          onClick={() => {
-            setType("motorista");
-          }}
-          className={`rounded-2xl py-4 px-6 gap-2 text-zinc-50 flex flex-col justify-center items-center  ${
-            type !== "motorista" ? "bg-zinc-200" : "bg-zinc-950"
-          }`}
-        >
-          <RadioGroupItem
-            value="motorista"
-            checked={type === "motorista"}
-            id="motorista"
-            className="hidden"
-          />
-          <div className="flex flex-col">
-            <h1
-              className={`text-zinc-50 text-center ${
-                type !== "motorista" ? "text-zinc-950" : "text-zinc-200"
-              }`}
-            >
-              Motorista
-            </h1>
-            <span
-              className={`text-zinc-400 text-xs text-center ${
-                type !== "motorista" ? "text-zinc-600" : "text-zinc-400"
-              }`}
-            >
-              Você veio oferecer caronas
-            </span>
-          </div>
-        </label>
-      </RadioGroup>
-      <Button className="py-6 cursor-pointer">Entrar</Button>
+      <div>
+        <label className="block mb-2">Tipo</label>
+        <RadioType type={type} setType={setType} />
+      </div>
+      <Button className="py-6 cursor-pointer">Cadastrar</Button>
     </form>
   );
 };
