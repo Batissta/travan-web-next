0f1d832ca80e7fd423ab672e1a314eb11b22a6db
Batissta <francinaldofilho05@gmail.com>
2025-04-04 20:31:58 -0300

    feat: ButtonChooseVehicle component

---
+++ src/components/home/ButtonChooseVehicle.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { motion } from "motion/react";
+import { Bus, BusFront, Car } from "lucide-react";
+
+type ChooseVehicleProps = {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  logo: "bus" | "van" | "car";
+};
+
+const ButtonChooseVehicle = ({ value, setValue, logo }: ChooseVehicleProps) => {
+  return (
+    <motion.button
+      whileHover={{
+        scale: 1.05,
+        y: -5,
+        rotate: "0deg",
+      }}
+      whileTap={{
+        scale: 0.95,
+        y: -0,
+        rotate: "11.25deg",
+      }}
+      onClick={() => setValue(logo)}
+      className={`p-4 border border-blue-200 rounded-2xl cursor-pointer text-blue-200 ${
+        value === logo && "bg-blue-200 text-blue-950"
+      }`}
+    >
+      {logo === "bus" && <Bus width={38} height={38} />}
+      {logo === "van" && <BusFront width={38} height={38} />}
+      {logo === "car" && <Car width={38} height={38} />}
+    </motion.button>
+  );
+};
+
+export default ButtonChooseVehicle;
