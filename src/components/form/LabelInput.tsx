0c983c8eb401bbe78fd5898d2360d61de709fbbc
Batissta <francinaldofilho05@gmail.com>
2025-04-05 23:09:05 -0300

    refactor: labelInput

--- src/components/form/LabelInput.tsx
+++ src/components/form/LabelInput.tsx
@@ -1,12 +1,13 @@
-import { IdCard, Key, Mail } from "lucide-react";
+import { IdCard, Key, Mail, MapPinned } from "lucide-react";
 import React from "react";
 
 type PropsLabelInput = {
-  label: string;
+  label?: string;
   htmlFor: string;
   type: string;
   placeholder?: string;
-  icon: "email" | "password" | "personality";
+  className?: string;
+  icon: "email" | "password" | "personality" | "pin";
 };
 
 const LabelInput = ({
@@ -15,10 +16,12 @@ const LabelInput = ({
   type,
   placeholder,
   icon,
+  className,
+  ...props
 }: PropsLabelInput) => {
   return (
     <div className="flex flex-col gap-2">
-      <label htmlFor={htmlFor}>{label}</label>
+      {label && <label htmlFor={htmlFor}>{label}</label>}
       <label
         htmlFor={htmlFor}
         className="border border-zinc-600 p-2 rounded relative flex gap-2 items-center text-zinc-700"
@@ -26,12 +29,16 @@ const LabelInput = ({
         {icon === "email" && <Mail width={16} height={16} />}
         {icon === "password" && <Key width={16} height={16} />}
         {icon === "personality" && <IdCard width={16} height={16} />}
+        {icon === "pin" && (
+          <MapPinned className={`${className}`} width={16} height={16} />
+        )}
         <input
-          className="placeholder:text-zinc-400 grow focus:outline-none"
+          className={`placeholder:text-zinc-400 grow focus:outline-none ${className}`}
           type={type}
           name={htmlFor}
           id={htmlFor}
           placeholder={placeholder && placeholder}
+          {...props}
         />
       </label>
     </div>
